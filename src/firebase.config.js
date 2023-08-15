d2c77aed70700c61b90162cbea6a76a1daf34478
Sam Muttai <42063584+muttaisammy@users.noreply.github.com>
2023-08-15 06:06:37 +0300

    aug 15

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,28 +3,20 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-/*
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOm4sm8rvHY9hJF-NAg61gsiB7EFtrEsw",
-  authDomain: "autotrac-77317.firebaseapp.com",
-  projectId: "autotrac-77317",
-  storageBucket: "autotrac-77317.appspot.com",
-  messagingSenderId: "213881391791",
-  appId: "1:213881391791:web:09d6c4420ea70e2541a25a"
+  apiKey: "AIzaSyAnrdEV5bFuXRUZJQ19uNVTR78oEfqEadg",
+  authDomain: "autostuff-766cd.firebaseapp.com",
+  projectId: "autostuff-766cd",
+  storageBucket: "autostuff-766cd.appspot.com",
+  messagingSenderId: "159375266211",
+  appId: "1:159375266211:web:1728402b8ca2b489630a16",
+  measurementId: "G-3JH8NBGBW1"
 };
 
-
-*/
-
 // orig
-
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyCI8RSCscuLKRqO0J624xDBBFDVqaCjBb0",
   authDomain: "cars-project-6cf91.firebaseapp.com",
@@ -35,7 +27,7 @@ const firebaseConfig = {
   measurementId: "G-HF59JYNXXV"
 };
 
-
+*/
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
